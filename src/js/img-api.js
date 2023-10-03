7edfac47ebf70129caf63a3e9bcbbce42ea5c689
Kreal11 <kirillshyrokov2@gmail.com>
2023-10-03 16:00:36 +0300

    start pagination

--- src/js/img-api.js
+++ src/js/img-api.js
@@ -4,6 +4,7 @@ const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '39794314-b7170df023ca4db44fdda06f6';
 
 export const fetchImages = async term => {
+  let page = 1;
   const response = await axios.get(BASE_URL, {
     params: {
       key: API_KEY,
@@ -11,6 +12,8 @@ export const fetchImages = async term => {
       image_type: 'photo',
       orientation: 'horizontal',
       safesearch: true,
+      per_page: 40,
+      page: page,
     },
   });
   return response.data.hits;
