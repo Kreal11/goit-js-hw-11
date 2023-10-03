4fd1246bccc539bc90b6404c257b0ad498bda3c2
Kreal11 <kirillshyrokov2@gmail.com>
2023-10-03 17:57:55 +0300

    done pagination

--- src/js/img-api.js
+++ src/js/img-api.js
@@ -3,17 +3,16 @@ import axios from 'axios';
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '39794314-b7170df023ca4db44fdda06f6';
 
-export const fetchImages = async term => {
-  let page = 1;
+export const fetchImages = async (searchQuery, page) => {
   const response = await axios.get(BASE_URL, {
     params: {
       key: API_KEY,
-      q: term,
+      q: searchQuery,
       image_type: 'photo',
       orientation: 'horizontal',
       safesearch: true,
       per_page: 40,
-      page: page,
+      page,
     },
   });
   return response.data.hits;
--- src/js/index.js
+++ src/js/index.js
@@ -3,49 +3,78 @@ import { fetchImages } from './img-api';
 
 const formEl = document.querySelector('.search-form');
 const divEl = document.querySelector('.gallery');
-const inputEl = document.querySelector('[name=searchQuery]');
+// const inputEl = document.querySelector('[name=searchQuery]');
+const loadMoreBtn = document.querySelector('.load-more');
 
-const onSubmitBtn = async event => {
-  event.preventDefault();
+formEl.addEventListener('submit', onSubmitBtn);
+loadMoreBtn.addEventListener('click', onLoadMoreData);
 
-  const term = inputEl.value;
+let page = 1;
+let totalAmountOfImages = 500;
+let amountPerPage = 40;
+let searchQuery = '';
+
+const renderPage = async () => {
+  const lastPage = Math.ceil(totalAmountOfImages / amountPerPage);
 
   try {
-    const images = await fetchImages(term);
+    const images = await fetchImages(searchQuery, page);
     if (images.length === 0) {
       return Notiflix.Notify.warning(
         'Sorry, there are no images matching your search query. Please try again.'
       );
     }
-    renderPage(images);
+    createMarkup(images);
+    loadMoreBtn.classList.remove('is-hidden');
+
+    if (page === lastPage) {
+      loadMoreBtn.classList.add('is-hidden');
+      Notiflix.Notify.warning(
+        'We are sorry, but you have reached the end of search results.'
+      );
+    }
   } catch (error) {
     Notiflix.Notify.failure('Oops, someting went wrong!');
   }
 };
 
-formEl.addEventListener('submit', onSubmitBtn);
-
-function renderPage(images) {
+function createMarkup(images) {
   const markup = images
     .map(image => {
       return `<div class="photo-card">
-  <img src="${image.webformatURL}" alt="Tags: ${image.tags}" loading="lazy" />
-  <div class="info">
+    <img src="${image.webformatURL}" alt="Tags: ${image.tags}" loading="lazy" width=250 height=200 />
+    <div class="info">
     <p class="info-item">
       <b>Likes: ${image.likes}</b>
-    </p>
-    <p class="info-item">
+      </p>
+      <p class="info-item">
       <b>Views: ${image.views}</b>
-    </p>
-    <p class="info-item">
+      </p>
+      <p class="info-item">
       <b>Comments: ${image.comments}</b>
     </p>
     <p class="info-item">
-      <b>Downloads: ${image.downloads}</b>
+    <b>Downloads: ${image.downloads}</b>
     </p>
-  </div>
-</div>`;
+    </div>
+    </div>`;
     })
     .join('');
   divEl.insertAdjacentHTML('beforeend', markup);
+}
+
+function onSubmitBtn(event) {
+  event.preventDefault();
+
+  page = 1;
+  searchQuery = event.target.elements.searchQuery.value.trim();
+  divEl.innerHTML = '';
+
+  renderPage();
+}
+
+function onLoadMoreData() {
+  page += 1;
+
+  renderPage();
 }
