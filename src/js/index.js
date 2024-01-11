94c6b3b8a10a351ead4345c46263c65e0d910e53
Kreal11 <kirillshyrokov2@gmail.com>
2024-01-11 17:14:18 +0200

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,123 +1,125 @@
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
-import Notiflix from 'notiflix';
-import { fetchImages, per_page } from './img-api';
+// import SimpleLightbox from 'simplelightbox';
+// import 'simplelightbox/dist/simple-lightbox.min.css';
+// import Notiflix from 'notiflix';
+// import { fetchImages, per_page } from './img-api';
 
-const formEl = document.querySelector('.search-form');
-const divEl = document.querySelector('.gallery');
-const loadMoreBtn = document.querySelector('.load-more');
-const inputEl = document.querySelector('[name=searchQuery]');
-const searchBtn = document.querySelector('.search-btn');
+// const formEl = document.querySelector('.search-form');
+// const divEl = document.querySelector('.gallery');
+// const loadMoreBtn = document.querySelector('.load-more');
+// const inputEl = document.querySelector('[name=searchQuery]');
+// const searchBtn = document.querySelector('.search-btn');
 
-formEl.addEventListener('submit', onSubmitBtn);
-loadMoreBtn.addEventListener('click', onLoadMoreData);
+// formEl.addEventListener('submit', onSubmitBtn);
+// loadMoreBtn.addEventListener('click', onLoadMoreData);
 
-let page = 1;
-let searchQuery = '';
-let submitClick = false;
+// let page = 1;
+// let searchQuery = '';
+// let submitClick = false;
 
-let gallery = new SimpleLightbox('.gallery a', {
-  captionsData: 'alt',
-  captionDelay: 250,
-});
+// let gallery = new SimpleLightbox('.gallery a', {
+//   captionsData: 'alt',
+//   captionDelay: 250,
+// });
 
-const renderPage = async () => {
-  try {
-    const images = await fetchImages(searchQuery, page);
+// const renderPage = async () => {
+//   try {
+//     const images = await fetchImages(searchQuery, page);
 
-    if (images.hits.length === 0) {
-      inputEl.disabled = false;
-      searchBtn.disabled = false;
-      return Notiflix.Notify.warning(
-        'Sorry, there are no images matching your search query. Please try again.'
-      );
-    }
+//     if (images.hits.length === 0) {
+//       inputEl.disabled = false;
+//       searchBtn.disabled = false;
+//       return Notiflix.Notify.warning(
+//         'Sorry, there are no images matching your search query. Please try again.'
+//       );
+//     }
 
-    if (submitClick) {
-      Notiflix.Notify.success(`Hooray! We found ${images.totalHits} images.`);
-    }
+//     if (submitClick) {
+//       Notiflix.Notify.success(`Hooray! We found ${images.totalHits} images.`);
+//     }
 
-    createMarkup(images.hits);
+//     createMarkup(images.hits);
 
-    gallery.refresh();
-    const { height: cardHeight } = document
-      .querySelector('.gallery')
-      .firstElementChild.getBoundingClientRect();
+//     gallery.refresh();
+//     const { height: cardHeight } = document
+//       .querySelector('.gallery')
+//       .firstElementChild.getBoundingClientRect();
 
-    window.scrollBy({
-      top: cardHeight * 2,
-      behavior: 'smooth',
-    });
+//     window.scrollBy({
+//       top: cardHeight * 2,
+//       behavior: 'smooth',
+//     });
 
-    loadMoreBtn.classList.remove('is-hidden');
+//     loadMoreBtn.classList.remove('is-hidden');
 
-    const lastPage = Math.ceil(images.totalHits / per_page);
+//     const lastPage = Math.ceil(images.totalHits / per_page);
 
-    if (page === lastPage) {
-      loadMoreBtn.classList.add('is-hidden');
-      Notiflix.Notify.info(
-        'We are sorry, but you have reached the end of search results.'
-      );
-    }
-  } catch (error) {
-    Notiflix.Notify.failure('Oops, someting went wrong!');
-  }
-  inputEl.disabled = false;
-  searchBtn.disabled = false;
-};
+//     if (page === lastPage) {
+//       loadMoreBtn.classList.add('is-hidden');
+//       Notiflix.Notify.info(
+//         'We are sorry, but you have reached the end of search results.'
+//       );
+//     }
+//   } catch (error) {
+//     Notiflix.Notify.failure('Oops, someting went wrong!');
+//   }
+//   inputEl.disabled = false;
+//   searchBtn.disabled = false;
+// };
 
-function createMarkup(images) {
-  const markup = images
-    .map(image => {
-      return `<a href="${image.largeImageURL}" class="photo-card">
-    <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" width=250 height=200 />
-    <div class="info">
-    <p class="info-item">
-      <b>👍 Likes: ${image.likes}</b>
-      </p>
-      <p class="info-item">
-      <b>👀 Views: ${image.views}</b>
-      </p>
-      <p class="info-item">
-      <b>💬 Comments: ${image.comments}</b>
-    </p>
-    <p class="info-item">
-    <b>⬇️ Downloads: ${image.downloads}</b>
-    </p>
-    </div>
-    </a>`;
-    })
-    .join('');
-  divEl.insertAdjacentHTML('beforeend', markup);
-}
+// function createMarkup(images) {
+//   const markup = images
+//     .map(image => {
+//       return `<a href="${image.largeImageURL}" class="photo-card">
+//     <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" width=250 height=200 />
+//     <div class="info">
+//     <p class="info-item">
+//       <b>👍 Likes: ${image.likes}</b>
+//       </p>
+//       <p class="info-item">
+//       <b>👀 Views: ${image.views}</b>
+//       </p>
+//       <p class="info-item">
+//       <b>💬 Comments: ${image.comments}</b>
+//     </p>
+//     <p class="info-item">
+//     <b>⬇️ Downloads: ${image.downloads}</b>
+//     </p>
+//     </div>
+//     </a>`;
+//     })
+//     .join('');
+//   divEl.insertAdjacentHTML('beforeend', markup);
+// }
 
-async function onSubmitBtn(event) {
-  event.preventDefault();
+// async function onSubmitBtn(event) {
+//   event.preventDefault();
 
-  submitClick = true;
+//   submitClick = true;
 
-  if (inputEl.value.trim() === '') {
-    Notiflix.Notify.warning('Please, fill the form! Form should not be empty!');
-    return;
-  }
+//   if (inputEl.value.trim() === '') {
+//     Notiflix.Notify.warning('Please, fill the form! Form should not be empty!');
+//     return;
+//   }
 
-  inputEl.disabled = true;
-  searchBtn.disabled = true;
+//   inputEl.disabled = true;
+//   searchBtn.disabled = true;
 
-  loadMoreBtn.classList.add('is-hidden');
+//   loadMoreBtn.classList.add('is-hidden');
 
-  page = 1;
-  searchQuery = event.target.elements.searchQuery.value.trim();
-  divEl.innerHTML = '';
+//   page = 1;
+//   searchQuery = event.target.elements.searchQuery.value.trim();
+//   divEl.innerHTML = '';
 
-  renderPage();
-}
+//   renderPage();
+// }
 
-function onLoadMoreData() {
-  page += 1;
-  loadMoreBtn.classList.add('is-hidden');
+// function onLoadMoreData() {
+//   page += 1;
+//   loadMoreBtn.classList.add('is-hidden');
 
-  submitClick = false;
+//   submitClick = false;
 
-  renderPage();
-}
+//   renderPage();
+// }
+
+console.log('hello');
